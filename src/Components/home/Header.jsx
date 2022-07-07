b890622d5fd3f341d11a991a02100736d96e58f7
Vanessa W <73565294+Macrina1984@users.noreply.github.com>
2022-07-07 14:51:41 -0400

    first Commit

---
+++ src/Components/home/Header.jsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+import { Link } from "react-router-dom";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import HomeIcon from '@mui/icons-material/Home'
+import GridViewIcon from "@mui/icons-material/GridView";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export const Header = () => {
+   const [sidebar, setSidebar] = useState(false);
+  return (
+    <>
+      <header className="header">
+        <div className="container flex div">
+          <div className="logo">Logo</div>
+          {/* <img src= "/blacks_in_classics_app/src/assets/images/logo.jpg" alt="logo"/> */}
+          <div className="nav">
+            <ul
+              className={sidebar ? "nav-links-sidebar" : "nav-links"}
+              onClick={() => setSidebar(false)}
+            >
+              <li>
+                {" "}
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/pages">Pages</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/About">About</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                {" "}
+                <Link to="/donate">Get Involved</Link>
+              </li>
+              <li className="icon">
+                <SearchOutlinedIcon className="HeaderIcon" />
+                <HomeIcon className="HeaderIcon" />
+                <GridViewIcon className="HeaderIcon" />
+              </li>
+            </ul>
+          </div>
+          <button className="navbar-items-icons" onClick={()=> setSidebar(!sidebar)}></button>
+             {sidebar ? <CloseIcon/> : <MenuIcon/>}
+        </div>
+      </header>
+    </>
+  );
+
+}
+// to do: add sublink to get involved-->Donate
+
+//How to use MUI---------------->
+//mui fix: npm install @mui/material @emotion/react @emotion/styled --legacy-peer-deps
+// npm install @mui/icons-material --legacy-peer-deps
+//import icons from: https://mui.com/material-ui/material-icons/?query=search&theme=Outlined
---
+++ src/Components/pageContent/HomePages.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const HomePages = () => {
+  return (
+    <><h1>
+        HomePages</h1></>
+  )
+}
